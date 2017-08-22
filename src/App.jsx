0bdf2eb279574aa3e7b071d119dcbc8e13d92c82
John Goff <gygn.johngoff@gmail.com>
2017-08-22 14:54:57 -0400

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React, {Component} from 'react';
+import Message from './Message.jsx';
+import ChatBar from './ChatBar.jsx';
+import MessageList from './MessageList.jsx';
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <MessageList><Message username={"maggie"} message={"test"} /></MessageList>
+        <ChatBar />
+      </div>
     );
   }
 }
---
+++ src/MessageList.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+class MessageList extends Component {
+  render() {
+    return (
+      <main className="messages">{this.props.children}</main>
+    );
+  }
+}
+export default MessageList;
