7ecd103f54283c1f7d92400ec293972684129d78
John Goff <gygn.johngoff@gmail.com>
2017-08-24 13:37:35 -0400

    Added image handling capabilities

--- src/Message.jsx
+++ src/Message.jsx
@@ -1,21 +1,43 @@
 import React, {Component} from 'react';
 
+const URI = require('urijs');
+
 class Message extends Component {
   render() {
+    let renderMessage = '';
     if (this.props.message.type === 'incomingMessage') {
-      return (
-        <div className="message">
-          <span className="message-username" style={{color: `#${this.props.message.color}`}}>{this.props.message.username || 'Anonymous'}</span>
-          <span className="message-content">{this.props.message.content}</span>
-        </div>
-      );
+      const imgs = [];
+      const parsed = URI.withinString(this.props.message.content,(url) => {
+        if (URI(url).suffix() === 'jpg' || URI(url).suffix() === 'png' || URI(url).suffix() === 'gif') {
+          imgs.push(url);
+          return '';
+        }
+      });
+      if (imgs.length === 0) {
+        renderMessage = (
+          <div className="message">
+            <span className="message-username" style={{color: `#${this.props.message.color}`}}>{this.props.message.username || 'Anonymous'}</span>
+            <span className="message-content">{parsed}</span>
+          </div>
+        );
+      } else {
+        const arr = imgs.map((href, idx) => <img src={href} key={idx} />);
+        renderMessage = (
+          <div className="message">
+            <span className="message-username" style={{color: `#${this.props.message.color}`}}>{this.props.message.username || 'Anonymous'}</span>
+            <span className="message-content">{parsed}</span>
+            {arr}
+          </div>
+        );
+      }
     } else if (this.props.message.type === 'incomingNotification') {
-      return (
+      renderMessage = (
         <div className="message system" style={{color: `#${this.props.message.color}`}}>
           {this.props.message.content}
         </div>
       );
     }
+    return renderMessage;
   }
 }
 export default Message;
